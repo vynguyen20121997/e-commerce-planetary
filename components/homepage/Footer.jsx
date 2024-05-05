ad9d06e4dbe523f62fcd967d90c191d2e156a0bc
vynguyen20121997 <133662722+vynguyen20121997@users.noreply.github.com>
2024-05-05 22:11:53 +0700

    update footer

--- components/homepage/Footer.jsx
+++ components/homepage/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-          Enter the Universe
+          VY NGUYEN PROJECT
         </h4>
       </div>
 
@@ -25,9 +25,9 @@ const Footer = () => (
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">METAVERUS</h4>
+          <h4 className="font-extrabold text-[24px] text-white">PLANETARY</h4>
           <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+            Copyright © 2024 All rights reserved.
           </p>
 
           <div className="flex gap-4">
